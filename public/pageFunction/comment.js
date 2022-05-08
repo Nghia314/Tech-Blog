1aaf241133eebb3d6bd769675f3c2de89119c0c5
Nghia Le <davidle314@hotmail.com>
2022-05-08 14:24:39 -0700

    comments function is finish allow user to make a comment and delete

---
+++ public/pageFunction/comment.js
@@ -0,0 +1,27 @@
+async function commentPostHandler(event) {
+    event.preventDefault();
+
+    const commentText= document.querySelector('input[name="post-comment-body"]').value.trim();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (commentText) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.string({
+                post_id, commentText
+            }),
+            headers: { 'Content-Type': 'application/json'}
+        });
+        if (response.status.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+            document.querySelector('#comment-form').style.display = 'block';
+        }
+    }
+}
+
+document.querySelector('#comment-form').addEventListener('submit',commentPostHandler);
