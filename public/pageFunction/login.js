00784939fb7c627899c3a7b07f9871deb08562dc
Nghia Le <davidle314@hotmail.com>
2022-05-08 02:00:50 -0700

    login javascrip is finish

---
+++ public/pageFunction/login.js
@@ -0,0 +1,22 @@
+async function loginHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                username, password
+            }),
+            headers: { 'Content-Type': 'application/json'}
+        });
+
+        if (response.status.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
