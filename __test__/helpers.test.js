87e711a580e587c5f3adf9645259ca064e71ed27
Nghia Le <davidle314@hotmail.com>
2022-05-09 13:44:48 -0700

    update time format

---
+++ __test__/helpers.test.js
@@ -0,0 +1,6 @@
+
+const { format_date } = require('../utils/helper');
+test('format_date() returns a date string', () => {
+    const date = new Date('2022-05-09 16:12:03');
+    expect(format_date(date)).toBe('5/9/2022');
+});
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -96,7 +96,7 @@ router.put('/:id', withAuth, (req, res) => {
         where: {
             id: req.params.id
         }
-    });
+    })
     .then(dbPostData => {
         if (!dbPostData) {
             res.status(404).json({ message: 'No post found with this id'});
